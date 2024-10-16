8ade491a856afdd9401d3ad624056213a1f899ca
Danilo <67532827+loloco90@users.noreply.github.com>
2024-10-16 19:44:50 -0300

    PJ 2.111

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ let xRaqueteOponente = 510, yRaqueteOponente = 150, velOponente = 5;
 let upPressed = false, downPressed = false;
 
 function setup() {
-    createCanvas(520, 520);
-
+  // Ajusta o canvas para 100% da largura da tela
+  let canvas = createCanvas(windowWidth * 0.8, windowHeight * 0.6);
   canvas.parent('gameContainer');  // Insere o canvas no div 'gameContainer'
 }
 
